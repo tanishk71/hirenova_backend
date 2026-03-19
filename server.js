2e437f2c6c2a17f27d7226cb4b4ff46221a3cae7
Tanishk Kamble <tanishkkamble1707@gmail.com>
2026-03-19 13:21:04 +0530

    Use Server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: process.env.FRONTEND_URL ,
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 };
 app.use(cors(corsOptions));
